117b4077b217b7acab409ecda7438564e50607d1
Rasoul Foroughfard <rossforoughi@gmail.com>
2026-04-05 14:04:25 -0700

    Remove top header name and center-style main title

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,15 +1,3 @@
-"use client";
-
-import Link from "next/link";
-
 export function Nav() {
-  return (
-    <header className="site-nav">
-      <div className="container">
-        <div className="site-title-row">
-          <Link href="/" className="site-title">Rasoul Foroughfard</Link>
-        </div>
-      </div>
-    </header>
-  );
+  return null;
 }
