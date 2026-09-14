a0d989fb728d1977a64d338970095bb6f48c3e52
Rasoul Foroughfard <rossforoughi@gmail.com>
2026-09-14 13:33:46 -0700

    Move email link to end

--- app/page.tsx
+++ app/page.tsx
@@ -103,10 +103,6 @@ export default function Home() {
           </p>
           <p className="bio contact-inline">
             <span className="contact-links">
-              <a className="icon-link" href="mailto:rforough@asu.edu" aria-label="Email rforough@asu.edu" title="Email">
-                <ContactIcon type="email" />
-                <span>Email</span>
-              </a>
               <a className="icon-link" href="/CV_Rasoul_ac.pdf" target="_blank" rel="noopener noreferrer" aria-label="Open CV" title="CV">
                 <ContactIcon type="cv" />
                 <span>CV</span>
@@ -118,6 +114,10 @@ export default function Home() {
               <a className="icon-link" href="https://www.linkedin.com/in/rasoul-foroughfard-342500b2" target="_blank" rel="noopener noreferrer" aria-label="LinkedIn" title="LinkedIn">
                 <ContactIcon type="linkedin" />
                 <span>LinkedIn</span>
+              </a>
+              <a className="icon-link" href="mailto:rforough@asu.edu" aria-label="Email rforough@asu.edu" title="Email">
+                <ContactIcon type="email" />
+                <span>Email</span>
               </a>
             </span>
             <br />
